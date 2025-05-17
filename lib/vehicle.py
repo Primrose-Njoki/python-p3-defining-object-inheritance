4d959b1522bbd2ef830e96c71400ffd9dda7d1f5
Primrose <primrose.njoki@student.moringaschool.com>
2025-05-17 12:08:33 +0300

    inherit

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,16 @@
 class Vehicle:
-    pass
+    def __init__(self,wheel_size,wheel_number):
+        self.wheel_size=wheel_size
+        self.wheel_number=wheel_number
+# instance methods
+    def go(self):
+        return "vrrrrrrrooom!"
+    
+    def fill_up_tank(self):
+        return "filling up!"
+
+my_car = Vehicle(18, 4)
+print(my_car.wheel_number)
+
+print(my_car.go())  
+print (my_car.fill_up_tank())
